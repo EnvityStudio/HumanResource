1197d4c97218e4c5ff10f0efedc43b8b63dab1da
khanhlq <khanh96le@gmail.com>
2017-03-19 17:23:52 +0700

    add UCPhongBan

--- HumanResource/GUI/UCPhongBan.Designer.cs
+++ HumanResource/GUI/UCPhongBan.Designer.cs
@@ -28,34 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtHoTen = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MANV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TEN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GIOI_TINH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NGAY_SINH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SODT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LOAI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PHONG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CHUCVU = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(218, 33);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(94, 20);
+            this.txtHoTen.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(277, 169);
+            this.label1.Location = new System.Drawing.Point(65, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Phong ban";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mã phòng ban";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MANV,
+            this.HO,
+            this.TEN,
+            this.GIOI_TINH,
+            this.NGAY_SINH,
+            this.SODT,
+            this.LOAI,
+            this.PHONG,
+            this.CHUCVU});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1270, 263);
+            this.dataGridView1.TabIndex = 72;
+            // 
+            // MANV
+            // 
+            this.MANV.DataPropertyName = "MANV";
+            this.MANV.HeaderText = "Mã";
+            this.MANV.Name = "MANV";
+            this.MANV.ReadOnly = true;
+            // 
+            // HO
+            // 
+            this.HO.DataPropertyName = "HO";
+            this.HO.HeaderText = "Họ";
+            this.HO.Name = "HO";
+            this.HO.ReadOnly = true;
+            // 
+            // TEN
+            // 
+            this.TEN.DataPropertyName = "TEN";
+            this.TEN.HeaderText = "Tên";
+            this.TEN.Name = "TEN";
+            this.TEN.ReadOnly = true;
+            // 
+            // GIOI_TINH
+            // 
+            this.GIOI_TINH.DataPropertyName = "GIOI_TINH";
+            this.GIOI_TINH.HeaderText = "Giới Tính";
+            this.GIOI_TINH.Name = "GIOI_TINH";
+            this.GIOI_TINH.ReadOnly = true;
+            // 
+            // NGAY_SINH
+            // 
+            this.NGAY_SINH.DataPropertyName = "NGAY_SINH";
+            this.NGAY_SINH.HeaderText = "Ngày Sinh";
+            this.NGAY_SINH.Name = "NGAY_SINH";
+            this.NGAY_SINH.ReadOnly = true;
+            // 
+            // SODT
+            // 
+            this.SODT.DataPropertyName = "SO_DT";
+            this.SODT.HeaderText = "Số Điện Thoại";
+            this.SODT.Name = "SODT";
+            this.SODT.ReadOnly = true;
+            // 
+            // LOAI
+            // 
+            this.LOAI.DataPropertyName = "LOAI_NV";
+            this.LOAI.HeaderText = "Loại";
+            this.LOAI.Name = "LOAI";
+            this.LOAI.ReadOnly = true;
+            // 
+            // PHONG
+            // 
+            this.PHONG.DataPropertyName = "PHONG";
+            this.PHONG.HeaderText = "Phòng";
+            this.PHONG.Name = "PHONG";
+            this.PHONG.ReadOnly = true;
+            // 
+            // CHUCVU
+            // 
+            this.CHUCVU.DataPropertyName = "CHUCVU";
+            this.CHUCVU.HeaderText = "Chức Vụ";
+            this.CHUCVU.Name = "CHUCVU";
+            this.CHUCVU.ReadOnly = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.txtHoTen);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(0, 17);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(1270, 267);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Thông Tin Phòng Ban";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 284);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1270, 263);
+            this.panel2.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1270, 17);
+            this.panel1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tên phòng ban";
             // 
             // UCPhongBan
             // 
             this.AccessibleDescription = "Phòng Ban";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "UCPhongBan";
-            this.Size = new System.Drawing.Size(755, 547);
+            this.Size = new System.Drawing.Size(1270, 547);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.TextBox txtHoTen;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MANV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TEN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GIOI_TINH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NGAY_SINH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SODT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LOAI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PHONG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CHUCVU;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
     }
 }
